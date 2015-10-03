34911b3a92bba855ec9180dcb8e4c861597f17e8
jakedavies <jgdavies24@gmail.com>
2015-10-02 17:58:25 -0700

    fuck you lua

--- main.lua
+++ main.lua
@@ -5,3 +5,6 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+local composer = require( "composer" )
+composer.gotoScene( "menu", { effect="crossFade", time=500 } )
