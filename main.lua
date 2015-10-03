d8f096faa6f2194698e0d31601dd2ee0b9f5832f
nkoriath <nkoriath@shaw.ca>
2015-10-02 19:51:05 -0700

    splash screen done

--- main.lua
+++ main.lua
@@ -7,6 +7,4 @@
 -- Your code here
 
 local composer = require( "composer" )
-=======
 composer.gotoScene( "game", { effect="crossFade", time=500 } )
->>>>>>> c8138cfdf4f96a6cd84fdc326109ad1edaab90d4
